d7f1c012bd771aff1e212358d48be328405a7e38
Gaurav 2248 <gaurav.more@nativebyte.in>
2025-03-15 00:20:44 +0530

    if condition in app.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,16 @@ const App = () => {
           const newTours = tours.filter(tour => tour.id !== id);
           setTours(newTours);
   } 
+
+  if(tours.length === 0) {
+    return(
+      <div className="refresh">
+        <h2>No Tours Left</h2>
+         <button onClick={()=> setTours(data)}>Refresh</button>   
+      </div>
+   
+    );
+  }
     return (
     
     

a63796cf5bc44fc24cba317e74704996679c68f7
Gaurav 2248 <gaurav.more@nativebyte.in>
2025-03-15 15:28:23 +0530

    added alt ti img tag

--- src/components/Card.js
+++ src/components/Card.js
@@ -13,7 +13,7 @@ function Card({id, image, info, price, name, removeTour}) {
      
     return (
         <div className="card">
-            <img src={image} className="image"></img>
+            <img src={image}  alt="Description of the image" className="image"></img>
 
             <div className="tour-info">
 
